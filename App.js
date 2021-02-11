a1c6ce4f758544112f50d0c1c422ba7f98aea4a1
agui1era <oscar@aguilera.xyz>
2021-02-11 12:27:40 -0300

    Update

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import {Image, StyleSheet, Text, View, TouchableHighlight, I18nManager } from 'react-native';
+import ImageLogo from './Img.js'
 
 export default function App() {
   let [KWH, setKWH] = React.useState('')
   let [pesos, setPesos] = React.useState('')
+  let [W, setPotencia] = React.useState('')
 
   const fetchApiCall = () => {
     fetch("http://iot.enerlife.cl:8000/energia/v1/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9", {
@@ -18,17 +20,36 @@ export default function App() {
       .catch(err => {
         console.log(err);
       });
-  }
+
+  fetch("http://iot.enerlife.cl:8000/potencia/v1/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9", {
+    "method": "GET",
+  })
+    .then(response => response.json())
+    .then(response => {
+      setPotencia(response.W);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
   return (
     <View style={styles.container}>
+      <ImageLogo/>
       <View>
+        <Text style={styles.title}>Generación Actual: {W} W</Text>
         <Text style={styles.title}>Energía generada del mes: {KWH} KWH</Text>
-        <Text style={styles.title}>Ahorro aprox: ${pesos}</Text>
+        <Text style={styles.title}>Ahorro aprox en el mes: ${pesos}</Text>
+        <Text style={styles.title}></Text>
       </View>
       <TouchableHighlight onPress={fetchApiCall}>
         <View style={styles.button}>
           <Text style={styles.buttonText}>Consultar</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Configurar Lector</Text>
         </View>
       </TouchableHighlight>
       <StatusBar style="auto" />
@@ -39,19 +60,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#AAA',
     alignItems: 'center',
     justifyContent: 'center',
-    color: '#fff'
+    color: '#008000'
   },
   title: {
     fontSize: 20,
-    color: '#fff'
+    color: '#008000'
   },
   button: {
-    padding: 10,
-    marginVertical: 15,
-    backgroundColor: '#0645AD'
+    marginRight:40,
+    marginLeft:40,
+    marginTop:10,
+    paddingTop:20,
+    paddingRight:20,
+    paddingLeft:20,
+    paddingBottom:20,
+    backgroundColor:'#FF6600',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff'
   },
   buttonText: {
     color: '#fff'
---
+++ Img.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { View, Image } from 'react-native'
+
+const ImagesExample = () => (
+   <Image source = {{uri:'https://images.assetsdelivery.com/compings_v2/wahyufrida/wahyufrida2001/wahyufrida200100175.jpg'}}
+   style = {{ width: 300, height: 300}}
+   />
+)
+export default ImagesExample
